28a8dd7b5c29ae0977929794549f095dc3dc1ef0
Hsing-Ho <hsinghochang@gmail.com>
2017-07-19 17:55:20 +0800

    Comments Module Edit
    Near to Complete Comments Module

--- modules.js
+++ modules.js
@@ -4,42 +4,81 @@
 	top:ele.top
 });
 */
-function comments(cont){
-	if (!cont.id) {
-		console.log(cont.id);
-		return;
-	}
-	var settings={
-		id:undefined,
-		left:0,
-		top:0
-	};
-	for(var attr in cont){
-		if (cont.hasOwnProperty(attr)) {
-			settings[attr] = cont[attr];
+function comments(e){//添加评论模块
+	document.onclick=null;
+	var body = document.querySelector('body');
+	var l = e.offsetX;
+	var t = e.offsetY;
+	l+400 > window.innerWidth ? l=l-400 : l = l;
+	t+300 > window.innerHeight ? t=t-300 : t = t;
+
+	var temp = document.createElement('section');
+	var div = document.createElement('div');
+	var input = document.createElement('input');
+	var text = document.createElement('textarea');
+	var button = document.createElement('button');
+	
+	temp.id = 'comments';
+	div.style.left=l+'px';
+	div.style.top = t+'px';
+	input.type='text';
+	input.name='name';
+	input.placeholder='Please Enter Your Name';
+	text.name='comments';
+	text.placeholder='Enter Message';
+	button.innerHTML='Comment';
+
+	div.appendChild(input);
+	div.appendChild(text);
+	div.appendChild(button);
+	temp.appendChild(div);
+	body.appendChild(temp);
+
+	temp.onclick=function(e){
+		event.cancelBubble = true;
+		if (e.target === temp) {
+			body.removeChild(temp);
+			temp.onclick=null;
+			document.onclick=comments;
 		}
-	}
-	var temp = document.createElement('div');
-	temp.id = 'commentBox';
-	temp.innerHTML= "<input type='text'><input type='button' value='Comment'>";
+	};
+	button.onclick=function(){
+		event.cancelBubble = true;
+		if (!input.value) {
+			alert('Please Enter Your Name!');
+			return;
+		}
+		if (!text.value) {
+			alert('Please Enter Your Comments!');
+			return;
+		}
+		// ajax();
+	};
+}
+
+function comList(ev){//返回留言信息列表
 	ajax({
 		data:{
-			action:'findOne',
-			parent:settings.id
+			// id:ev.targetNode.id
 		},
+		action:'findOne',
 		succ:function(res){
+			n=0;
 			for (var i = 0; i < res.comments.length; i++) {
 				var li = document.createElement('li');
-				li.innerHTML = res.comments[i];
-				temp.appendChild(li);
+				li.innerHTML=res.comments[i].name+': '+res.comments[i].comment;
+				ul.appendChild(li);
 			}
+			div.appendChild(ul);
+			temp.appendChild(div);
+			document.querySelector('body').appendChild(temp);
 		}
 	});
 }
 
 function ajax(json){
 	var settings={
-		url:'http://www.rhythmer.club/login',
+		url:'/login',
 		method:'post',
 		data:{},
 		dataType:'json',

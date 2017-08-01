3f72ca1e43ceaba945d476a5cb07e398f5196f0b
hsingho <hsinghochang@gmail.com>
2017-08-02 00:25:44 +0800

    update ajax>success funtion

--- rhythmer/modules.js
+++ rhythmer/modules.js
@@ -1,148 +1,3 @@
-/*comments({
-	id:ele.id,
-	left:ele.left,
-	top:ele.top
-});
-*/
-function comments(e){//添加评论模块
-	event.cancelBubble=true;
-	var body = document.querySelector('body');
-	var l = e.offsetX;
-	var t = e.offsetY;
-	l+400 > window.innerWidth ? l=l-400 : l = l;
-	t+300 > window.innerHeight ? t=t-300 : t = t;
-
-	var temp = document.createElement('section');
-	var div = document.createElement('div');
-	var input = document.createElement('input');
-	var text = document.createElement('textarea');
-	var button = document.createElement('button');
-	
-	temp.id = 'comments';
-	div.style.left=l+'px';
-	div.style.top = t+'px';
-	input.type='text';
-	input.name='name';
-	input.placeholder='Please Enter Your Name';
-	text.name='comments';
-	text.placeholder='Enter Message';
-	button.innerHTML='GO';
-
-	div.appendChild(input);
-	div.appendChild(button);
-	div.appendChild(text);
-	temp.appendChild(div);
-	body.appendChild(temp);
-
-	temp.onclick=function(e){
-		event.cancelBubble = true;
-		if (e.target === temp) {
-			body.removeChild(temp);
-			temp.onclick=null;
-			document.onclick=comments;
-		}
-	};
-	button.onclick=function(){
-		event.cancelBubble = true;
-		if (!input.value) {
-			alert('Please Enter Your Name!');
-			return;
-		}
-		if (!text.value) {
-			alert('Please Enter Your Comments!');
-			return;
-		}
-		ajax({
-			url:'/save',
-			data:{
-				name:input.value,
-				message:text.value,
-				comments:{}
-			},
-			succ:function(){
-				alert('Success!');
-			}
-		});
-	};
-}
-
-function comList(e,setEle){//返回留言信息列表
-	event.cancelBubble = true;
-
-	var body = document.querySelector('body');
-	var thisID = e.target.getAttribute('comID');
-	var l = e.clientX;
-	l+400 > window.innerWidth ? l=l-400 : l = l;
-
-	var temp = document.createElement('section');
-	var div = document.createElement('div');
-	var input = document.createElement('input');
-	var text = document.createElement('textarea');
-	var button = document.createElement('button');
-	
-	temp.id = 'comments';
-	div.style.left=l+'px';
-	div.style.top = (window.innerHeight - 600)/2+'px';
-	div.className = 'comment'
-	input.type='text';
-	input.name='name';
-	input.placeholder='Please Enter Your Name';
-	text.name='comments';
-	text.placeholder='Enter Message';
-	button.innerHTML='GO';
-
-	div.appendChild(input);
-	div.appendChild(button);
-	div.appendChild(text);
-
-	var ul = document.createElement('ul');
-	if (commentsAll[thisID].length) {
-		for (var i = commentsAll[thisID].length-1; i>=0 ; i--) {
-			var li = document.createElement('li');
-			li.innerHTML=commentsAll[thisID][i].name+': '+commentsAll[thisID][i].message;
-			ul.appendChild(li);
-		}
-	}else{
-		ul.innerHTML = '<li>暂无留言</li>';
-	}
-	div.appendChild(ul);
-
-	temp.appendChild(div);
-	body.appendChild(temp);
-
-	temp.onclick=function(e){
-		event.cancelBubble = true;
-		if (e.target === temp) {
-			body.removeChild(temp);
-			temp.onclick=null;
-			move(setEle);
-		}
-	};
-	button.onclick=function(){
-		event.cancelBubble = true;
-		if (!input.value) {
-			alert('Please Enter Your Name!');
-			return;
-		}
-		if (!text.value) {
-			alert('Please Enter Your Comments!');
-			return;
-		}
-		ajax({
-			url:'/update',
-			data:{
-				_id:thisID,
-				comments:JSON.stringify({"name":input.value,"message":text.value})
-			},
-			succ:function(res){
-				console.log(res);
-				alert('Success!');
-			}
-		});
-	};
-}
-
-
 /*
 mod(e, list, callback)
 	e 			->	e			| 	event infomation
@@ -178,11 +33,11 @@ Callback of save msg:
 			})
 		}
 */
-function mod(e, list, callback){
-	event.cancelBubble = true;
+function mod(e, title, callback, setEle){
 	var body = document.querySelector('body'),
 		temp = document.createElement('section'),
 		div = document.createElement('div'),
+		h3 = document.createElement('h3');
 		input = document.createElement('input'),
 		text = document.createElement('textarea'),
 		button = document.createElement('button');
@@ -194,6 +49,8 @@ function mod(e, list, callback){
 		event.cancelBubble = true;
 		if (e.target === temp) {
 			body.removeChild(temp);
+			temp.onclick = null;
+			setEle && move(setEle);
 		}
 	};
 	/*Submit button event creating*/
@@ -213,23 +70,28 @@ function mod(e, list, callback){
 
 	temp.id = 'comments';
 	div.className = 'comment';
+	h3.innerHTML = title;
 	input.type = 'text';
 	input.name = 'name';
 	input.placeholder = 'Please Enter Your Name';
+	input.tabIndex = 1;
 	text.name = 'comments';
 	text.placeholder = 'Please Enter Your Message';
+	text.tabIndex = 2;
 	button.innerHTML = 'GO';
+	button.tabIndex = 3;
 
+	div.appendChild(h3);
 	div.appendChild(input);
 	div.appendChild(button);
 	div.appendChild(text);
 
-	if (list) {
+	if (setEle) {
 		var ul = document.createElement('ul');
 		if (commentsAll[thisID].length) {
-			for (var i = commentsAll.length-1; i>=0; i--){
+			for (var i = commentsAll[thisID].length-1; i>=0; i--){
 				var li = document.createElement('li');
-				li.innerHTML = commentsAll[thisID][i].name+":"+commentsAll[thisID][i].message;
+				li.innerHTML = '【'+(i+1)+'】'+commentsAll[thisID][i].name+"：“"+commentsAll[thisID][i].message+"”";
 				ul.appendChild(li);
 			}
 		}else{
@@ -244,6 +106,8 @@ function mod(e, list, callback){
 	t = t+div.offsetHeight>window.innerHeight ? window.innerHeight-div.offsetHeight : t;
 	div.style.left = l + 'px';
 	div.style.top = t + 'px';
+
+	input.focus();
 }
 
 
